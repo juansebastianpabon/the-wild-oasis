c4681b20eb4a14ccf14178a59dcb35d6b2242d02
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-10-26 22:18:56 -0500

    Añade la funcionalidad para subir la imagen de la cabaña al bucket de supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,48 +9,19 @@ import { useMutation } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import { useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import FormRow from "../../ui/FormRow";
 
 function CreateCabinForm() {
   const queryClient = useQueryClient();
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm();
 
-  const { mutate, isLoading: isDeleting } = useMutation({
+  const { mutate, isPending: isCreating } = useMutation({
     mutationFn: createCabin,
     onSuccess: () => {
       toast.success("New Cabin created successfully");
@@ -63,62 +34,93 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
+  }
+  function onError(errors) {
+    /*  console.log(errors); */
   }
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor='name'>Cabin name</Label>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow
+        label='Cabin name'
+        error={errors?.name?.message}>
         <Input
           type='text'
           id='name'
-          {...register("name")}
+          disabled={isCreating}
+          {...register("name", { required: "It's required" })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='maxCapacity'>Maximum capacity</Label>
+      <FormRow
+        label='Maximum capacity'
+        error={errors?.maxCapacity?.message}>
         <Input
           type='number'
           id='maxCapacity'
-          {...register("maxCapacity")}
+          disabled={isCreating}
+          {...register("maxCapacity", {
+            required: "It's required",
+            min: {
+              value: 1,
+              message: "Capacity must be at least 1",
+            },
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='regularPrice'>Regular price</Label>
+      <FormRow
+        label='Regular price'
+        error={errors?.regularPrice?.message}>
         <Input
           type='number'
           id='regularPrice'
-          {...register("regularPrice")}
+          min={0}
+          disabled={isCreating}
+          {...register("regularPrice", { required: "It's required" })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='discount'>Discount</Label>
+      <FormRow
+        label='Discount'
+        error={errors?.discount?.message}>
         <Input
           type='number'
           id='discount'
+          disabled={isCreating}
+          min={0}
           defaultValue={0}
-          {...register("discount")}
+          {...register("discount", {
+            required: "It's required",
+
+            validate: (value) =>
+              Number(value) <= Number(getValues().regularPrice) ||
+              "Discount must be less than regular price",
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='description'>Description for website</Label>
+      <FormRow
+        label='Description'
+        error={errors?.description?.message}>
         <Textarea
           type='number'
           id='description'
+          disabled={isCreating}
           defaultValue=''
-          {...register("description")}
+          {...register("description", { required: "It's required" })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='image'>Cabin photo</Label>
+      <FormRow
+        label='Image'
+        disabled={isCreating}
+        error={errors?.image?.message}>
         <FileInput
           id='image'
           accept='image/*'
+          type='file'
+          {...register("image", { required: "It's required" })}
         />
       </FormRow>
 
@@ -129,7 +131,7 @@ function CreateCabinForm() {
           type='reset'>
           Cancel
         </Button>
-        <Button disabled={isDeleting}>Edit cabin</Button>
+        <Button disabled={isCreating}>Edit cabin</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
@@ -22,14 +22,35 @@ export async function deleteCabin(id) {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabins/${imageName}`;
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
+  }
+
+  //upload image to storage
+  const { error: storageError } = await supabase.storage
+    .from("cabins")
+    .upload(imageName, newCabin.image);
+
+  //if storage error, delete the cabin entry
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
