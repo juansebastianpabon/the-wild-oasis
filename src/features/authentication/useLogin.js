f6b984b8907cadc51ba6be3d40293ead19ee1945
juansebastianpabon <nova159712@gmail.com>
2025-12-01 08:55:14 -0500

    Añade el logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,18 @@
 import { login as loginApi } from '../../services/apiAuth';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      navigate('/dashboard');
+      queryClient.setQueryData(['user'], user.user);
+      navigate('/dashboard', { replace: true });
     },
     onError: (error) => {
-      console.log('ERRO', error);
+      console.log('ERROR', error);
       toast.error('Provided email or password are incorrect.  ');
     },
   });
