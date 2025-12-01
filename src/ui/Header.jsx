4432c876074b47861fdf163c5af41439c971e5d6
juansebastianpabon <nova159712@gmail.com>
2025-12-01 18:24:23 -0500

    Añade el header de la app

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components';
 import LogOut from '../features/authentication/LogOut';
+import HeaderMenu from './HeaderMenu';
+import UserAvatar from '../features/authentication/UserAvatar';
 const StyleHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 function Header() {
   return (
     <StyleHeader>
-      <LogOut />
+      <UserAvatar />
+      <HeaderMenu />
     </StyleHeader>
   );
 }
